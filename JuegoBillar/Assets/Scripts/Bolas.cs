06f2e86b6815e7f1a7f6135a6915be333accdc98
marrazki <martin_leoz@euneiz.com>
2025-03-02 02:43:49 +0100

    PrefabsBolas
    Prefabs de todas las bolas creados

---
+++ JuegoBillar/Assets/Scripts/Bolas.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bolas : MonoBehaviour
+{
+    //public bool[] bolas = new bool [15];
+    //bool[] bolas = [false, true, false, true, false, true, false, true, false, true, false, true, false, true, false];
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
